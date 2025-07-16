eeb705a36a84a045da7b6cd2caa39c2c47e9c6a4
Shivcodes91 <shivangrai238@gmail.com>
2025-07-16 11:39:24 +0530

    solving

--- javascript/even_odd.js
+++
@@ -1,9 +0,0 @@
-function checkOddEven(num) {
-    if ((num & 1) === 0) {
-        console.log(num + " is Even");
-    } else {
-        console.log(num + " is Odd");
-    }
-}
-
-checkOddEven(7);//1,2,1,3,4,3,4
--- javascript/xor.js
+++
@@ -1,8 +0,0 @@
-let x=[1,2,1,3,4,3,4];
-let y=0;
-for(let i =0;i<x.length;i++){
-    y=y^x[i];
-}
-console.log(y);
-
-
